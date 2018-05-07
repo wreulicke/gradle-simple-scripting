dd4f43e8d469a2f72c06901901a644de72686c2d
wreulicke <saito.masaya@classmethod.jp>
2018-05-07 15:26:23 +0900

    init project

---
+++ src/main/java/com/github/wreulicke/modules/Module.java
@@ -0,0 +1,13 @@
+package com.github.wreulicke.modules;
+
+
+/**
+ * Module
+ */
+public class Module {
+
+    public static String getVersion() {
+        return "1.0.0";
+    }
+    
+}
---
+++ src/script/java/com/github/wreulicke/script/Main.java
@@ -0,0 +1,13 @@
+package com.github.wreulicke.script;
+
+import com.github.wreulicke.modules.Module;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Module.getVersion());
+    }
+}
